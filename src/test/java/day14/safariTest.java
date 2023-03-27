9a91cf47bb60b22a56764ecd6a867c9fd3966096
hieu.vu <vutrunghieu.smartl@gmail.com>
2023-03-27 16:34:07 +0700

    Feature: adding safari test #1 - add safari test on macOs-latest

---
+++ src/test/java/day14/safariTest.java
@@ -0,0 +1,42 @@
+package day14;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class safariTest {
+    By toWebdriverPage = By.xpath("//a[@href='/documentation/webdriver/']");
+    String webDriverPageTitle = "WebDriver | Selenium";
+
+    @BeforeClass
+    public void beforeEachClass (){
+        System.out.println("Summary: Run safari on macOs-latest");
+    }
+
+
+
+    @Test
+    public void safariBrowserTest(){
+        WebDriverManager.safaridriver().setup();
+        SafariOptions options = new SafariOptions();
+        options.setAutomaticInspection(false);
+        options.setAutomaticProfiling(false);
+        WebDriver driver = new SafariDriver(options);
+        driver.get("https://www.selenium.dev/");
+        driver.findElement(toWebdriverPage).click();
+        Assert.assertEquals(driver.getTitle(), webDriverPageTitle);
+        driver.quit();
+    }
+
+    @AfterClass
+    public void afterEachClass(){
+        // Method runs lastly
+        System.out.println("Done");
+    }
+}

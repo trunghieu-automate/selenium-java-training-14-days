89862ef9e83d898d63d8b63c514f90afe3102919
hieu.vu <vutrunghieu.smartl@gmail.com>
2023-03-27 17:01:15 +0700

    Feature: adding safari test/#3 - fix test.yml file and fix SafariTest.java

--- src/test/java/day14/safariTest.java
+++ src/test/java/day14/safariTest.java
@@ -10,33 +10,39 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class safariTest {
     By toWebdriverPage = By.xpath("//a[@href='/documentation/webdriver/']");
     String webDriverPageTitle = "WebDriver | Selenium";
+    WebDriver driver;
+
 
     @BeforeClass
     public void beforeEachClass (){
-        System.out.println("Summary: Run safari on macOs-latest");
-    }
-
-
-
-    @Test
-    public void safariBrowserTest(){
         WebDriverManager.safaridriver().setup();
         SafariOptions options = new SafariOptions();
         options.setAutomaticInspection(false);
         options.setAutomaticProfiling(false);
-        WebDriver driver = new SafariDriver(options);
+        driver = new SafariDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        System.out.println("Run safari browser test");
+
+    }
+
+    @Test
+    public void safariBrowserTest(){
         driver.get("https://www.selenium.dev/");
         driver.findElement(toWebdriverPage).click();
         Assert.assertEquals(driver.getTitle(), webDriverPageTitle);
-        driver.quit();
+
     }
 
     @AfterClass
     public void afterEachClass(){
         // Method runs lastly
+        driver.quit();
         System.out.println("Done");
     }
 }
